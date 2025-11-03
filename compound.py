b1cd103c586af40abfd306eadb6a70f1bea7d127
Trisha B <t@ameyab.com>
2025-11-02 20:08:47 -0800

    simple test 2

--- compound.py
+++ compound.py
@@ -15,8 +15,22 @@ def call_llm(message):
   )
   return (response.choices[0].message.content, response.choices[0].message.executed_tools)
 
+def visit_website(message):
+  response = groq_client.chat.completions.create(
+    messages=[
+      {
+        "role": "user",
+        "content": message
+      }
+    ],
+    model="groq/compound-mini"
+  )
+  return (response.choices[0].message.content, response.choices[0].message.executed_tools)
+
 
 if __name__ == "__main__":
-  response, executed_tools = call_llm("What is the highest grossing movie this week?")
-  print(response)
+  # response, executed_tools = call_llm("What is the highest grossing movie this week?")
+  # print(response)
   # print(executed_tools)
+  response, executed_tools = call_llm("Summarize the key points of this page: https://tbhatawdekar.github.io/")
+  print(response)
